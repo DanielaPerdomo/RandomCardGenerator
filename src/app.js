2010d7fcd1d9c5e720171fa805331b66d8947196
DanielaPerdomo <Daniela.perdomo.perez@gmail.com>
2023-06-30 16:39:03 +0000

    Proyecto completo

--- src/app.js
+++ src/app.js
@@ -2,10 +2,55 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let simbolos = ["diamante", "picas", "corazon", "trebol"];
+  let numeros = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  cardGenerator(numeros, simbolos);
+  document.querySelector(".nuevacarta").addEventListener("click", () => {
+    document
+      .querySelector(".card")
+      .classList.remove("diamante", "picas", "corazon", "trebol");
+    cardGenerator(numeros, simbolos);
+  });
+  setInterval(() => {
+    document
+      .querySelector(".card")
+      .classList.remove("diamante", "picas", "corazon", "trebol");
+    cardGenerator(numeros, simbolos);
+  }, 10000);
+};
+
+function cardGenerator(numeros, simbolos) {
+  let card = document.querySelector(".card");
+  let randomSimbol = generateRandomSimbol(simbolos);
+  if (randomSimbol === "corazon" || randomSimbol === "diamante") {
+    card.style.color = "red";
+  } else {
+    card.style.color = "black";
+  }
+  card.classList.add(randomSimbol);
+  card.innerHTML = generateRandomNumero(numeros);
+}
+let generateRandomNumero = numeros => {
+  let indexNumero = Math.floor(Math.random() * numeros.length);
+  return numeros[indexNumero];
+};
+let generateRandomSimbol = simbolos => {
+  let indexSimbol = Math.floor(Math.random() * simbolos.length);
+  return simbolos[indexSimbol];
 };
